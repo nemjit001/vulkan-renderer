712d9a899ca15792a19ff4d3fde351e638d149aa
Tijmen Verhoef <tijmenmenno@gmail.com>
2024-11-16 16:36:37 +0100

    added texture + samplers to pipeline layout & created textures. Loading is still TODO

--- src/main.cpp
+++ src/main.cpp
@@ -156,6 +156,7 @@ namespace Engine
     std::vector<VkFramebuffer> swapFramebuffers{};
 
     // Per pass data
+    VkSampler textureSampler = VK_NULL_HANDLE;
     VkDescriptorSetLayout descriptorSetLayout = VK_NULL_HANDLE;
     VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
     VkPipeline graphicsPipeline = VK_NULL_HANDLE;
@@ -171,6 +172,12 @@ namespace Engine
     Camera camera{};
     Transform transform{};
     Mesh mesh{};
+
+    // Material data
+    Texture colorTexture{};
+    VkImageView colorTextureView;
+    Texture normalTexture{};
+    VkImageView normalTextureView;
 
     // CPU side render data
     void* pSceneData = nullptr;
@@ -991,16 +998,54 @@ namespace Engine
             }
         }
 
-        // Create descriptor set layouts, pipeline layout, and graphics pipeline
+        // Create samplers, descriptor set layouts, pipeline layout, and graphics pipeline
         {
+            VkSamplerCreateInfo samplerCreateInfo{ VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO };
+            samplerCreateInfo.flags = 0;
+            samplerCreateInfo.magFilter = VK_FILTER_LINEAR;
+            samplerCreateInfo.minFilter = VK_FILTER_LINEAR;
+            samplerCreateInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+            samplerCreateInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+            samplerCreateInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+            samplerCreateInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+            samplerCreateInfo.mipLodBias = 0.0F;
+            samplerCreateInfo.anisotropyEnable = VK_FALSE;
+            samplerCreateInfo.maxAnisotropy = 0.0F;
+            samplerCreateInfo.compareEnable = VK_FALSE;
+            samplerCreateInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+            samplerCreateInfo.minLod = 0.0F;
+            samplerCreateInfo.maxLod = VK_LOD_CLAMP_NONE;
+            samplerCreateInfo.borderColor = VK_BORDER_COLOR_FLOAT_OPAQUE_BLACK;
+            samplerCreateInfo.unnormalizedCoordinates = VK_FALSE;
+
+            if (VK_FAILED(vkCreateSampler(device, &samplerCreateInfo, nullptr, &textureSampler)))
+            {
+                printf("Vulkan texture sampler create failed\n");
+                return false;
+            }
+
             VkDescriptorSetLayoutBinding sceneDataUniformBinding{};
             sceneDataUniformBinding.binding = 0;
             sceneDataUniformBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
             sceneDataUniformBinding.descriptorCount = 1;
-            sceneDataUniformBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+            sceneDataUniformBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
             sceneDataUniformBinding.pImmutableSamplers = nullptr;
 
-            VkDescriptorSetLayoutBinding descriptorSetLayoutBindings[] = { sceneDataUniformBinding, };
+            VkDescriptorSetLayoutBinding colorTextureBinding{};
+            colorTextureBinding.binding = 1;
+            colorTextureBinding.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+            colorTextureBinding.descriptorCount = 1;
+            colorTextureBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+            colorTextureBinding.pImmutableSamplers = &textureSampler;
+
+            VkDescriptorSetLayoutBinding normalTextureBinding{};
+            normalTextureBinding.binding = 2;
+            normalTextureBinding.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+            normalTextureBinding.descriptorCount = 1;
+            normalTextureBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+            normalTextureBinding.pImmutableSamplers = &textureSampler;
+
+            VkDescriptorSetLayoutBinding descriptorSetLayoutBindings[] = { sceneDataUniformBinding, colorTextureBinding, normalTextureBinding, };
             VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo{ VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO };
             descriptorSetLayoutCreateInfo.flags = 0;
             descriptorSetLayoutCreateInfo.bindingCount = SIZEOF_ARRAY(descriptorSetLayoutBindings);
@@ -1253,7 +1298,7 @@ namespace Engine
             }
         }
 
-        // Set up scene data
+        // Set up scene data & load mesh
         {
             camera.position = glm::vec3(0.0F, 0.0F, -5.0F);
             camera.forward = glm::normalize(glm::vec3(0.0F) - camera.position);
@@ -1267,6 +1312,79 @@ namespace Engine
             }
         }
 
+        // Load material data
+        {
+            if (!createTexture(
+                colorTexture,
+                device,
+                VK_IMAGE_TYPE_2D,
+                VK_FORMAT_R8G8B8A8_UNORM,
+                VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+                VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                512, 512, 1
+            ))
+            {
+                printf("Vulkan color texture create failed\n");
+                return false;
+            }
+
+            if (!createTexture(
+                normalTexture,
+                device,
+                VK_IMAGE_TYPE_2D,
+                VK_FORMAT_R8G8B8A8_UNORM,
+                VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+                VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                512, 512, 1
+            ))
+            {
+                printf("Vulkan normal texture create failed\n");
+                return false;
+            }
+
+            VkImageViewCreateInfo colorTextureViewCreateInfo{ VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO };
+            colorTextureViewCreateInfo.flags = 0;
+            colorTextureViewCreateInfo.image = colorTexture.handle;
+            colorTextureViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            colorTextureViewCreateInfo.format = colorTexture.format;
+            colorTextureViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            colorTextureViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            colorTextureViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            colorTextureViewCreateInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            colorTextureViewCreateInfo.subresourceRange.baseMipLevel = 0;
+            colorTextureViewCreateInfo.subresourceRange.levelCount = colorTexture.levels;
+            colorTextureViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+            colorTextureViewCreateInfo.subresourceRange.layerCount = colorTexture.depthOrLayers;
+            colorTextureViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+
+            if (VK_FAILED(vkCreateImageView(device, &colorTextureViewCreateInfo, nullptr, &colorTextureView)))
+            {
+                printf("Vulkan color texture view create failed\n");
+                return false;
+            }
+
+            VkImageViewCreateInfo normalTextureViewCreateInfo{ VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO };
+            normalTextureViewCreateInfo.flags = 0;
+            normalTextureViewCreateInfo.image = normalTexture.handle;
+            normalTextureViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            normalTextureViewCreateInfo.format = normalTexture.format;
+            normalTextureViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            normalTextureViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            normalTextureViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            normalTextureViewCreateInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            normalTextureViewCreateInfo.subresourceRange.baseMipLevel = 0;
+            normalTextureViewCreateInfo.subresourceRange.levelCount = normalTexture.levels;
+            normalTextureViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+            normalTextureViewCreateInfo.subresourceRange.layerCount = normalTexture.depthOrLayers;
+            normalTextureViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+
+            if (VK_FAILED(vkCreateImageView(device, &normalTextureViewCreateInfo, nullptr, &normalTextureView)))
+            {
+                printf("Vulkan color texture view create failed\n");
+                return false;
+            }
+        }
+
         // Update descriptor sets
         {
             VkDescriptorBufferInfo sceneDataBufferInfo{};
@@ -1274,15 +1392,42 @@ namespace Engine
             sceneDataBufferInfo.offset = 0;
             sceneDataBufferInfo.range = sceneDataBuffer.size;
 
-            VkWriteDescriptorSet writeDescriptorSet{ VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET };
-            writeDescriptorSet.dstSet = descriptorSet;
-            writeDescriptorSet.dstBinding = 0;
-            writeDescriptorSet.dstArrayElement = 0;
-            writeDescriptorSet.descriptorCount = 1;
-            writeDescriptorSet.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-            writeDescriptorSet.pBufferInfo = &sceneDataBufferInfo;
+            VkDescriptorImageInfo colorTextureInfo{};
+            colorTextureInfo.sampler = VK_NULL_HANDLE;
+            colorTextureInfo.imageView = colorTextureView;
+            colorTextureInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 
-            vkUpdateDescriptorSets(device, 1, &writeDescriptorSet, 0, nullptr);
+            VkDescriptorImageInfo normalTextureInfo{};
+            normalTextureInfo.sampler = VK_NULL_HANDLE;
+            normalTextureInfo.imageView = normalTextureView;
+            normalTextureInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+
+            VkWriteDescriptorSet sceneDataWrite{ VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET };
+            sceneDataWrite.dstSet = descriptorSet;
+            sceneDataWrite.dstBinding = 0;
+            sceneDataWrite.dstArrayElement = 0;
+            sceneDataWrite.descriptorCount = 1;
+            sceneDataWrite.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+            sceneDataWrite.pBufferInfo = &sceneDataBufferInfo;
+
+            VkWriteDescriptorSet colorTextureWrite{ VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET };
+            colorTextureWrite.dstSet = descriptorSet;
+            colorTextureWrite.dstBinding = 1;
+            colorTextureWrite.dstArrayElement = 0;
+            colorTextureWrite.descriptorCount = 1;
+            colorTextureWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+            colorTextureWrite.pImageInfo = &colorTextureInfo;
+
+            VkWriteDescriptorSet normalTextureWrite{ VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET };
+            normalTextureWrite.dstSet = descriptorSet;
+            normalTextureWrite.dstBinding = 2;
+            normalTextureWrite.dstArrayElement = 0;
+            normalTextureWrite.descriptorCount = 1;
+            normalTextureWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+            normalTextureWrite.pImageInfo = &normalTextureInfo;
+
+            VkWriteDescriptorSet descriptorWrites[] = { sceneDataWrite, colorTextureWrite, normalTextureWrite, };
+            vkUpdateDescriptorSets(device, SIZEOF_ARRAY(descriptorWrites), descriptorWrites, 0, nullptr);
         }
 
         printf("Initialized Vulkan Renderer\n");
@@ -1295,6 +1440,11 @@ namespace Engine
 
         vkWaitForFences(device, 1, &frameReady, VK_TRUE, UINT64_MAX);
 
+        vkDestroyImageView(device, normalTextureView, nullptr);
+        normalTexture.destroy();
+        vkDestroyImageView(device, colorTextureView, nullptr);
+        colorTexture.destroy();
+
         mesh.destroy();
 
         vkDestroyDescriptorPool(device, descriptorPool, nullptr);
@@ -1304,6 +1454,7 @@ namespace Engine
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
+        vkDestroySampler(device, textureSampler, nullptr);
 
         for (auto& framebuffer : swapFramebuffers) {
             vkDestroyFramebuffer(device, framebuffer, nullptr);
--- src/shaders/forward.frag.glsl
+++ src/shaders/forward.frag.glsl
@@ -1,6 +1,9 @@
 #version 460
 #pragma shader_stage(fragment)
 
+#define INV_GAMMA   2.2
+#define GAMMA       1.0 / INV_GAMMA
+
 layout(location = 0) in FS_IN
 {
 	vec3 vertexPos;
@@ -11,8 +14,39 @@ layout(location = 0) in FS_IN
 
 layout(location = 0) out vec4 out_FragColor;
 
+layout(set = 0, binding = 0) uniform SCENE_DATA_UNIFORM
+{
+	vec3 cameraPosition;
+	mat4 viewproject;
+	mat4 model;
+	mat4 normal;
+};
+
+layout(set = 0, binding = 1) uniform sampler2D colorTexture;
+layout(set = 0, binding = 2) uniform sampler2D normalTexture;
+
+vec3 ambientLight = vec3(0.0);
+vec3 sunColor = vec3(1.0);
+float specularity = 0.75;
+
 void main()
 {
-	vec3 N = normalize(in_Input.TBN * vec3(0, 0, 1));
-	out_FragColor = vec4(0.5 + 0.5 * N, 1);
+	vec3 color = pow(texture(colorTexture, in_Input.texCoord).rgb, vec3(INV_GAMMA)); // Convert from SRGB to linear colors    
+    vec3 normal = texture(normalTexture, in_Input.texCoord).rgb; // Assume normals stored in linear format
+    normal = (2.0 * normal) - 1.0;
+
+	vec3 L = normalize(vec3(0, 1, -1)); // TODO(nemjit001): add to uniform data
+    vec3 V = normalize(cameraPosition - in_Input.vertexPos);
+    vec3 H = normalize(L + V);
+    vec3 N = normalize(in_Input.TBN * normal);
+
+	float NoL = clamp(0.0, 1.0, dot(N, L));
+    float NoH = clamp(0.0, 1.0, dot(N, H));
+
+	vec3 ambient = ambientLight * color;
+    vec3 diffuse = NoL * color * sunColor;
+    vec3 specular = pow(NoH, 64.0) * sunColor;
+    vec3 outColor = ambient + diffuse + specularity * specular; // Blend material based on constant
+
+	out_FragColor = vec4(outColor, 1);
 }
